1117daa6ab70a8794df559934caeb3a81218639c
aniruddha2007 <66187064+aniruddha2007@users.noreply.github.com>
2021-09-01 07:09:53 +0800

    Create mouse_demo.py

---
+++ Demos/mouse_demo.py
@@ -0,0 +1,16 @@
+#imports
+from pymouse import PyMouse
+from pykeyboard import PyKeyboard
+
+#set mouse and keyboard as variables
+m = PyMouse()
+k = PyKeyboard()
+
+#set mouse coordinates according to the screen_size
+x_dim, y_dim = m.screen_size()
+
+#click mouse at the selected position
+m.click(x_dim/2, y_dim/2, 1)
+
+#enter text using keyboard variable
+k.type_string('#This is a Test Message!')
